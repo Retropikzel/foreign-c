3183946989aa30a79e7dcad7e4cd85b056b794e9
retropikzel <retropikzel@iki.fi>
2025-06-25 08:29:50 +0300

    Switch to new compile-r7rs

--- foreign/c/libc.scm
+++ foreign/c/libc.scm
@@ -1,10 +1,13 @@
 (cond-expand
-  (windows (define-c-library libc '("stdio.h" "string.h") "ucrtbase" '()))
+  (windows (define-c-library libc
+                             '("stdlib.h" "stdio.h" "string.h")
+                             "ucrtbase"
+                             '()))
   (else
-    ;(define c-library "c")
-    #;(when (get-environment-variable "BE_HOST_CPU")
+    (define c-library "c")
+    (when (get-environment-variable "BE_HOST_CPU")
       (set! c-library "root"))
     (define-c-library libc
-                      '("stdio.h" "string.h")
+                      '("stdlib.h" "stdio.h" "string.h")
                       "c"
                       '((additional-versions ("0" "6"))))))
