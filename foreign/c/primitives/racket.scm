146de7843b3a9e79b3a7b11ae287b19a489f71f1
retropikzel <retropikzel@iki.fi>
2025-07-23 12:09:55 +0300

    Racket uint8-ref/set! fixes

--- foreign/c/primitives/racket.scm
+++ foreign/c/primitives/racket.scm
@@ -59,11 +59,11 @@
 
 (define c-bytevector-u8-set!
   (lambda (c-bytevector k byte)
-    (ptr-set! c-bytevector _ubyte k byte)))
+    (ptr-set! c-bytevector _ubyte 'abs k byte)))
 
 (define c-bytevector-u8-ref
   (lambda (c-bytevector k)
-    (ptr-ref c-bytevector _ubyte k)))
+    (ptr-ref c-bytevector _ubyte 'abs k)))
 
 (define c-bytevector-pointer-set!
   (lambda (c-bytevector k pointer)
