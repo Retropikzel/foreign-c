ececb7906aba3f8ec6166121f0105880730943b0
retropikzel <retropikzel@iki.fi>
2025-08-05 08:06:36 +0300

    Fixing stklos

--- foreign/c/pointer.scm
+++ foreign/c/pointer.scm
@@ -76,6 +76,8 @@
 
 (define c-utf8->string
   (lambda (c-bytevector)
+    (display c-bytevector)
+    (newline)
     (when (c-null? c-bytevector)
       (error "Can not turn null pointer into string" c-bytevector))
     (let ((size (c-strlen c-bytevector)))
