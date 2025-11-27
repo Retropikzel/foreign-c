48bc21bb290b2959e4fc5cbddc91d39e11e5a8b3
retropikzel <retropikzel@iki.fi>
2025-11-27 20:16:12 +0200

    Adding more tests

--- tests/c-bytevector-sint-ref.scm
+++ tests/c-bytevector-sint-ref.scm
@@ -1,6 +1,6 @@
 (test-begin "c-bytevector-sint-ref")
 
-(test-equal (c-bytevector-sint-ref sint-cbv 0) 32 (native-endianness) (c-type-size 'int))
+(test-equal (c-bytevector-sint-ref sint-cbv 0 (native-endianness) (c-type-size 'int)) 32)
 
 (test-end "c-bytevector-sint-ref")
 
