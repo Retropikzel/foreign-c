08e5a2ab9fa1a064ee0d1b1af783fd2f04666324
retropikzel <retropikzel@iki.fi>
2026-08-29 13:25:48 +0300

    Bump version. Git-index

--- .snow-repo.scm
+++ .snow-repo.scm
@@ -1,6 +1,113 @@
 (repository
   (package
     (git
+      (hash "8f217bcb841d07b2f86dd2c37c9977070cd3c300")
+      (url "https://codeberg.org/foreign-c/foreign-c.git"))
+    (authors "Retropikzel")
+    (version "0.23.4")
+    (library
+      (name
+        (foreign c))
+      (path "foreign/c.sld")
+      (foreign-depends "ffi")
+      (cond-expand
+        (capy
+          (depends
+            (core foreign)
+            (core foreign-library)))
+        (chezscheme
+          (depends
+            (chezscheme)
+            (chezscheme)))
+        (chibi
+          (depends
+            (chibi)))
+        (chicken
+          (depends
+            (chicken base)
+            (chicken foreign)
+            (chicken locative)
+            (chicken syntax)
+            (chicken memory)
+            (chicken random)))
+        (gauche
+          (depends
+            (gauche keyword)
+            (gauche base)
+            (gauche ffi)
+            (gauche uvector)
+            (gauche native-type)
+            (gauche ffi native)
+            (gauche ffi ffiaux)))
+        (guile
+          (depends
+            (scheme base)
+            (scheme write)
+            (scheme char)
+            (scheme file)
+            (scheme process-context)
+            (scheme inexact)
+            (system foreign)
+            (system foreign-library)
+            (rnrs bytevectors)))
+        (ikarus
+          (depends
+            (ikarus foreign)))
+        (ironscheme
+          (depends
+            (ironscheme clr)
+            (ironscheme clr internal)
+            (ironscheme ffi)))
+        (kawa
+          (depends
+            (kawa reflect)))
+        (mosh
+          (depends
+            (mosh ffi)))
+        (racket
+          (depends
+            (ffi winapi)
+            (compatibility mlist)
+            (ffi unsafe)
+            (ffi vector)))
+        (sagittarius
+          (depends
+            (sagittarius ffi)))
+        (stklos
+          (depends
+            (stklos)))
+        (ypsilon
+          (depends
+            (ypsilon c-ffi)
+            (ypsilon c-types)
+            (core))))
+      (cond-expand
+        (chicken
+          (depends))
+        (else
+          (depends)))
+      (cond-expand
+        (capy
+          (depends))
+        (chicken
+          (depends))
+        (kawa
+          (depends))
+        (else
+          (depends)))
+      (depends
+        (scheme base)
+        (scheme write)
+        (scheme char)
+        (scheme file)
+        (scheme process-context)
+        (scheme inexact)
+        (scheme cxr)))
+    (manual "foreign/c.html")
+    (description "Portable foreign function interface for R6RS and R7RS Schemes")
+    (updated "2026-08-29T13:26:10+00:00"))
+  (package
+    (git
       (hash "42fab0ece4a374b6b384069b64dadc786408cb64")
       (url "https://codeberg.org/foreign-c/foreign-c.git"))
     (authors "Retropikzel")
